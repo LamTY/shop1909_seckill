234d240eb5c8f7956fa1877cc2d7f6c1fdba264b
TY <657695528@qq.com>
2019-12-30 01:51:12 +0800

    第3次 feign

--- shop_gateway/src/main/java/com/qf/shop_gateway/ShopGatewayApplication.java
+++ shop_gateway/src/main/java/com/qf/shop_gateway/ShopGatewayApplication.java
@@ -2,8 +2,10 @@ package com.qf.shop_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ShopGatewayApplication {
 
     public static void main(String[] args) {
--- shop_microservice/shop_back/src/main/java/com/qf/controller/GoodsController.java
+++ shop_microservice/shop_back/src/main/java/com/qf/controller/GoodsController.java
@@ -1,10 +1,26 @@
 package com.qf.controller;
 
-import org.springframework.stereotype.Controller;
+import com.qf.entity.Goods;
+import com.qf.feign.GoodsFeign;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("")
-@Controller
+import java.util.List;
+
+@RequestMapping("/goods")
+@RestController
 public class GoodsController {
+
+    @Autowired
+    private GoodsFeign goodsFeign;
+
+    @RequestMapping("/list")
+    public String list(Model model){
+        List<Goods> goodsList = goodsFeign.goodsList();
+        model.addAttribute("goodsList", goodsList);
+        return "goodslist";
+    }
 
 }
