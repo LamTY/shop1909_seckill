737da204417a87a269ae8c55b0c29e02925982dd
TY <12345@qq.com>
2020-01-10 20:57:59 +0800

    第12-2次 商品提醒功能，客户端和服务器连接完成

---
+++ shop_message/src/test/java/com/qf/shop_message/ShopMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qf.shop_message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
